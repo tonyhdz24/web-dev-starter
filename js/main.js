8ad841b40000aac319ebfa8f952075147ddc5d34
Tony Hernandez <tonyhernandez24@u.boisestate.edu>
2024-08-26 09:46:25 -0600

    Added white space

--- js/main.js
+++ js/main.js
@@ -4,9 +4,9 @@ window.onload = loaded;
  * Simple Function that will be run when the browser is finished loading.
  */
 function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+  // Assign to a variable so we can set a breakpoint in the debugger!
+  const hello = sayHello();
+  console.log(hello);
 }
 
 /**
@@ -14,5 +14,5 @@ function loaded() {
  * @return {string} the string hello
  */
 export function sayHello() {
-    return 'hello';
+  return "hello";
 }
